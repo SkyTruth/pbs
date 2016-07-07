810758eb46e0595a952ade791431a34b1b88f6cc
Kevin Wurster <kevin@skytruth.org>
2016-07-07 16:36:25 -0400

    Make pandoc an optional install

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+
+import itertools as it
+
 from setuptools import setup
 
 try:
@@ -12,6 +15,27 @@ except IOError:
 except ImportError:
     print("warning: pypandoc module not found, could not convert Markdown to RST")
     long_description = ""
+
+
+install_requires = [
+    'click',
+    'pybossa-client>=1.0.4, <1.0.5',
+    'requests',
+    'nose',
+    'mock',
+    'coverage',
+    'rednose',
+    'simplejson',
+    'jsonschema',
+    'polib',
+    'watchdog']
+
+
+extras_require = {
+    'docs': ['pypandoc']
+}
+extras_require['all'] = list(
+    it.chain.from_iterable(extras_require.values())) + install_requires
 
 setup(
     name="pybossa-pbs",
@@ -29,8 +53,8 @@ setup(
                    'Operating System :: OS Independent',
                    'Programming Language :: Python',],
     py_modules=['pbs', 'helpers', 'pbsexceptions'],
-    install_requires=['click', 'pybossa-client>=1.0.4, <1.0.5', 'requests', 'nose', 'mock', 'coverage',
-                      'rednose', 'pypandoc', 'simplejson', 'jsonschema', 'polib', 'watchdog'],
+    install_requires=install_requires,
+    extras_require=extras_require,
     entry_points='''
         [console_scripts]
         pbs=pbs:cli
